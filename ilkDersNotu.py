e0c7f5be8442ecc6ac5161f008e3980edf8752ff
DEM1R <77392252+AbdullahDEM1R@users.noreply.github.com>
2022-11-04 12:32:22 +0300

    Update ilkDersNotu.py

--- ilkDersNotu.py
+++ ilkDersNotu.py
@@ -14,7 +14,7 @@ print (metin.strip()) #sağdaki ve soldaki boşlukları siler. eğer lstrip ders
 print (metin.count('a')) #belirtilen karakterden kaç tane geçtiği gösterilir.
 print (metin.split()) #boşluk karakterinden sonra ayırır veya eğer içine değer atarsak o değer gördüğünde ayırır.
 
-                                                                                ###FORMATLAMA###
+                                                                ###FORMATLAMA###
 
 print ("Merhaba benim adım {}. {} yaşındayım".format(metin, yas)) #burada formatın içine yazılan değişkenler süslü parantez ile belirtilen yere sırasıyla gelir. eğer süslü parantezlerin içine 0 1 2 gibi sayılar yazarsak önce soldan başlayarak koymak yerine sayıya göre koyar -> {1}{0}{0}, yas, metin --->> abdullah demir 19 19 yazar.
 print ("Merhaba benim adım {m}. {y} . {y} yıldır yaşıyorum".format(m = metin, y = yas)) #m gördüğü yere metini y gödüğü yere yas değişkenini atar. Bu işleme geçici değişken atama diyebiliriz.
@@ -28,4 +28,19 @@ liste = [1, 1231, 1234123, "abdullah", 123122, "demir"]
 sayilar.sort() # burada sayilar listesini sıraladık
 print(sayilar)
 sayilar.reverse() # burada sayilar listesini tersten sıraladk.
-print (liste.index("abdullah")) # abdullah değişkeninin indexini bize gösterir.
+print (liste.index("abdullah")) # abdullah değişkeninin indexini bize gösterir
+
+liste.append("muhammed") #en son kısma muhammed stringini ekler
+liste.insert(2, "ayten") #2. index yerine ayten stringini ekler ve sağdaki değerler birer tane sağa gider.
+liste.pop() #en sondaki elemanı siler yani muhammed stringi silinir.
+liste.clear() #listedeki elemanları siler ve boş liste olarak kaydeder.
+del liste #listeyi tamamen siler, ramde tutulmaz.
+
+# Eğer bir listede stringin karakterlerine erişmek istersek önce listeden stringi çekip sonrasında da karakteri belirtmemiz lazım.
+## Örnek -> liste = ("abdullah", "demir") 
+## print (liste[1][3]) #önce demir değerine girer ve sonrasında 3. indexi yani i'yi çeker. Bu durum sayılarda geçersizdir.
+
+liste3 = liste.copy()
+liste2 = liste
+liste.append("demir")
+print(liste2) # bu durumda liste2'nin en sonunda demir stringi de eklenir ve eğer bu durumdan etkilenmesini istemiyorsak liste2 = liste.copy() yapılır.
