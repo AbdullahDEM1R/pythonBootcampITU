ad41cd21d16c1caac8ba5e797b5d85a583a183ea
DEM1R <77392252+AbdullahDEM1R@users.noreply.github.com>
2022-11-06 23:57:52 +0300

    Update task1_medium.py

--- odevler/ikinciHaftaOdevleri/task1_medium.py
+++ odevler/ikinciHaftaOdevleri/task1_medium.py
@@ -1,1 +1,31 @@
+"""
+Kullanıcıdan iki sayı ve bir karakter isteyin. 
+Kullanıcıdan alınan sayıların genişliği ve uzunluğunda alınan karakterlerden oluşan bir dikdörtgen çizdirin.
+!! Bu programı döngüler kullanmadan yapınız
 
+EX:
+s1 -> 5
+s2 -> 7
+c1 -> *
+
+[OUTPUT]
+
+*****
+*****
+*****
+*****
+*****
+*****
+*****
+""" # task1_medium.pdf dosyasında görselli açıklama bulunuyor
+
+satir = int(input("Oluşturmak istediğiniz dörtgenin satır sayısını giriniz -->> "))
+sutun = int(input("Oluşturmak istediğiniz dörtgenin sutun sayısını giriniz -->> "))
+karakter = input("Oluşturmak istediğiniz dörtgenin hangi karakterden oluşmasını istediğinizi giriniz -->> ")
+
+
+liste = []
+liste.append((("\t" + karakter * sutun) + "\n") * (satir -1))
+liste.append(("\t" + karakter * sutun))
+
+print(*liste)
